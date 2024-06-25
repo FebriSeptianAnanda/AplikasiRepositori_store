d6a2389869bbb981a6e36b104c5ea74024f08db2
FebriSeptianAnanda <febriseptianananda@gmail.com>
2024-06-25 17:07:55 +0700

    first commit

---
+++ application/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Copyright © <a href="https://www.instagram.com/feb5659/" target="_blank">Febri Septian Ananda</a></span>
+		</div>
+	</div>
+</footer>
